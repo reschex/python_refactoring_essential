985259656f504bd07e2d20a4230072cc187de5dc
Roland Eschenburg <roland.eschenburg@hotmail.co.uk>
2026-06-16 19:40:52 +0100

    It works!

--- comments/src/X1.py
+++ comments/src/X1.py
@@ -5,12 +5,10 @@ class X1:
         acc_sum = 0
 
         for i in range(start, end + 1):
-            # Add square of each number in the range
             acc_sum += X1.calculate_square(i)
 
         return acc_sum
 
     @staticmethod
-    def calculate_square(k):
-        # Return square of input
-        return k * k
+    def calculate_square(num):
+        return num * num
