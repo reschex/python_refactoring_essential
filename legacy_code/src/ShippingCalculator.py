aa8f9a63fb2310bee3fac292d84803d9ded59939
Roland Eschenburg <roland.eschenburg@hotmail.co.uk>
2026-06-16 21:00:07 +0100

    Order interface to make stuff testable

--- legacy_code/src/ShippingCalculator.py
+++ legacy_code/src/ShippingCalculator.py
@@ -15,20 +15,7 @@ class ShippingCalculator:
 
     def calculate_shipping(self, order_id: int) -> float:
         try:
-            url = f"https://codemanship.co.uk/api/orders.php?orderId={order_id}"
-
-            response = requests.get(url)
-            response.raise_for_status()
-
-            data = response.json()
-
-            order = Order(
-                orderId=data["orderId"],
-                shippingType=data["shippingType"],
-                weightKg=data["weightKg"],
-                distanceKm=data["distanceKm"],
-                fragile=data["fragile"]
-            )
+            order = GetOrderFromUrl.get_order(order_id)
 
             if order.shippingType == "STANDARD":
                 return order.weightKg * 0.5
@@ -45,3 +32,43 @@ class ShippingCalculator:
         except Exception as e:
             print(e)
             return -1.0
+
+
+class GenericOrderGetter:
+    @staticmethod
+    def get_order(order_id):
+        pass
+
+
+class GetOrderFromUrl(GenericOrderGetter):
+    @staticmethod
+    def get_order(order_id):
+        url = f"https://codemanship.co.uk/api/orders.php?orderId={order_id}"
+
+        response = requests.get(url)
+        response.raise_for_status()
+
+        data = response.json()
+
+        order = Order(
+                orderId=data["orderId"],
+                shippingType=data["shippingType"],
+                weightKg=data["weightKg"],
+                distanceKm=data["distanceKm"],
+                fragile=data["fragile"]
+            )
+
+        return order
+
+
+class TestOrderGetter(GenericOrderGetter):
+    @staticmethod
+    def get_order(order_id):
+        
+        match order_id:
+            case 1001: 
+                return Order(orderId=1001, shippingType="STANDARD", weightKg=5, distanceKm=120, fragile=False)
+            case 1002: 
+                return Order(orderId=1002, shippingType="EXPRESS", weightKg=8.5, distanceKm=300, fragile=True)
+            case 1003: 
+                return Order(orderId=1003, shippingType="OVERNIGHT", weightKg=2, distanceKm=50, fragile=False)
---
+++ legacy_code/test/test_ShippingCalculator.py
@@ -0,0 +1,7 @@
+import unittest
+
+from legacy_code.src.ShippingCalculator import ShippingCalculator
+
+
+class TestShippingCalculator(unittest.TestCase):
+    pass
