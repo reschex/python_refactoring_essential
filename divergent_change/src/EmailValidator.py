480ee4a06e057b9bcadb66fb22f49d1c65ce8137
Sam Taggart <sam@sasworkshops.com>
2026-06-16 13:28:11 -0600

    It works!

--- divergent_change/src/EmailValidator.py
+++ divergent_change/src/EmailValidator.py
@@ -1,2 +1,6 @@
 class EmailValidator:
-    pass
+    def is_valid_email(self, email: str) -> bool:
+        if email is None:
+            return False
+        pattern = r"^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$"
+        return re.match(pattern, email) is not None
