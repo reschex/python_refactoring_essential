7f5d0bc8a9b01c879bd3f89935bb19715e5d7414
Roland Eschenburg <roland.eschenburg@hotmail.co.uk>
2026-06-16 19:30:51 +0100

    It works!

--- comments/src/X1.py
+++ comments/src/X1.py
@@ -2,15 +2,15 @@ class X1:
 
     @staticmethod
     def m(start, end):
-        p = 0
+        acc_sum = 0
 
         # Iterate from lower bound (q) to upper bound (z)
         for i in range(start, end + 1):
             # Add square of each number in the range
-            p += X1.n(i)
+            acc_sum += X1.n(i)
 
         # Return accumulated sum
-        return p
+        return acc_sum
 
     @staticmethod
     def n(k):
